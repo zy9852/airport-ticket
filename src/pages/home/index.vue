d3b420bc636a99cd027a8fee3ffa2cb2ae74b0a6
zy9852 <zx9852@foxmail.com>
2019-04-08 15:02:59 +0800

    添加全局flex布局设置

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div class="home">this is home</div>
+  <div class="home fx-row fx-v-center fx-m-between">
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped>
+@import '../../stylus/common.styl';
+.home {
+  background-color: #ccc;
+  .box {
+    width: 100px;
+    height: 50px;
+    background-color: skyblue;
+  }
+}
+</style>
